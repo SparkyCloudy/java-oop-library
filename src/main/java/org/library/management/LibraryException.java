b1b15882343a4005555387bc1fbb3a4f857285ae
prajna <satriagama1@gmil.com>
2023-05-01 21:09:45 +0800

    Initial commit

---
+++ src/main/java/org/library/management/LibraryException.java
@@ -0,0 +1,9 @@
+package org.library.management;
+
+public class LibraryException extends Exception{
+	public LibraryException(String message) {
+		super(message);
+	}
+
+	public LibraryException() {}
+}
