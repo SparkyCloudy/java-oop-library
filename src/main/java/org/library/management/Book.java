057e9ef0dd62e85518d83bcfe6d02af00cc7f78a
prajna <satriagama1@gmail.com>
2023-05-02 12:19:17 +0800

    changed to record class

--- src/main/java/org/library/management/Book.java
+++ src/main/java/org/library/management/Book.java
@@ -1,20 +1,3 @@
 package org.library.management;
 
-public class Book {
-
-  private final String id;
-  private final String title;
-
-  public Book(String title, String id) {
-    this.title = title;
-    this.id = id;
-  }
-
-  public String id() {
-    return id;
-  }
-
-  public String title() {
-    return title;
-  }
-}
+public record Book(String title, String id) {}
