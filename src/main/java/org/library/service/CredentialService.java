c3776d6cb3f9dbdcad23fd649a4a9fd2d4a75d7a
Prajna Paramahansa <satriagama1@gmail.com>
2023-06-25 21:39:05 +0800

    feat(service): add login check and status method

--- src/main/java/org/library/service/CredentialService.java
+++ src/main/java/org/library/service/CredentialService.java
@@ -1,6 +1,10 @@
 package org.library.service;
 
+import java.sql.SQLException;
+
 public interface CredentialService {
 
-  boolean loginCheck(String username, String password);
+  boolean loginCheck(String username, String password) throws SQLException;
+  
+  boolean getLoginStatus();
 }
--- src/main/java/org/library/service/CredentialServiceImpl.java
+++ src/main/java/org/library/service/CredentialServiceImpl.java
@@ -1,10 +1,16 @@
 package org.library.service;
 
 import org.library.repository.CredentialRepository;
+import org.library.utils.ConnectionUtil;
+import org.library.utils.LibraryException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class CredentialServiceImpl implements CredentialService {
 
   private final CredentialRepository credentialRepository;
+  private static int loginCounter = 0;
 
   public CredentialServiceImpl(CredentialRepository credentialRepository) {
     this.credentialRepository = credentialRepository;
@@ -12,9 +18,25 @@ public class CredentialServiceImpl implements CredentialService {
 
   @Override
   public boolean loginCheck(String username, String password) {
-
-    while (!username.equals()) {
-
+    String sql = String.format("""
+        SELECT * FROM user WHERE username = '%s' AND password = '%s'
+        """, username, password);
+    
+    try (ResultSet rs = ConnectionUtil.resultSet(sql)) {
+      if (rs.next()) {
+        if (rs.getRow() > 0) return true;
+      }
+    } catch (SQLException e) {
+      LibraryException.getError(e);
     }
+    
+    CredentialServiceImpl.loginCounter++;
+    
+    return false;
+  }
+  
+  @Override
+  public boolean getLoginStatus() {
+    return CredentialServiceImpl.loginCounter >= 3;
   }
 }
